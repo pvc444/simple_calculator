58bbbe37c544470ae9fd6a3425e2c10c8d1222cc
Milo <livanimahyar@gmail.com>
2026-09-02 11:35:32 +0330

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,38 +1,56 @@
-while True:
-  try:
-    num1 = float(input("enter the first number:"))
-    num2 = float(input("enter the second number:"))
+def calculator(num1, num2, operators):
+    if operators == "+":
+        return num1 + num2
 
-    while True:
-      operators = input("enter one of the followings: + ,- ,/ ,*.")
-      if operators == "+":
-        print(num1 + num2)
-        break
-      elif operators == "-":
-        print(num1 - num2)
-        break
-      elif operators == "*":
-         print(num1 * num2)
-         break
-      elif operators == "/":
+    elif operators == "-":
+        return num1 - num2
+
+    elif operators == "*":
+        return num1 * num2
+
+    elif operators == "/":
         if num2 != 0:
-           print(num1 / num2) 
+            return num1 / num2
         else:
-          print("cant be divided by zero")
-        break
-      else:
-        print("operators not available")
+            return "cant be divided by zero"
 
-    again = input("do want to calculate again:(y/n)").lower()
-  
+while True:
+
+# گرفتن عدد اول
+    while True:
+        try:
+            num1 = float(input("enter the first number:"))
+            break
+        except ValueError:
+            print("thats not a number! please enter a number")
+
+# گرفتن عدد دوم
+    while True:
+        try:
+            num2 = float(input("enter the second number:"))
+            break
+        except ValueError:
+            print("thats not a number! please enter a number")
+
+# گرفتن عملگر
+    while True:
+        operators = input("enter one of the followings: +, -, /, *.")
+
+        if operators == "+" or operators == "-" or operators == "*" or operators == "/":
+            result = calculator(num1, num2, operators)
+            print(result)
+            break
+
+        else:
+            print("operators not available")
+
+# دوباره اجرا کردن
+    again = input("do you want to calculate again? (y/n): ").lower()
+
     while again != "y" and again != "n":
-      print("please enter y or n!")
-      again = input("do want to calculate again:(y/n)").lower()
-      
+        print("please enter y or n!")
+        again = input("do you want to calculate again? (y/n): ").lower()
+
     if again == "n":
-       print("Goodbye")
-       break
-      
-  except ValueError:
-    print("thats not a number!please enter a number")
-    continue
+        print("Goodbye")
+        break
